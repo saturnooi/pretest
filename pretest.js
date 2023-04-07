7b0701efb33c468cb1dd98244de75546757b0dca
์Nuttawat <83624530+saturnooi@users.noreply.github.com>
2023-04-07 18:11:50 +0700

    Add files via upload

--- pretest.js
+++ pretest.js
@@ -9,19 +9,15 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
 };
 function merge(collection_1, collection_2) {
     var max = Math.max.apply(Math, __spreadArray(__spreadArray([], collection_1, false), collection_2, false));
-    // create a counting array to keep track of how many times each number appears
     var counts = new Array(max + 1).fill(0);
-    // count the number of times each number appears in collection_1
     for (var _i = 0, collection_1_1 = collection_1; _i < collection_1_1.length; _i++) {
         var num = collection_1_1[_i];
         counts[num]++;
     }
-    // count the number of times each number appears in collection_2
     for (var _a = 0, collection_2_1 = collection_2; _a < collection_2_1.length; _a++) {
         var num = collection_2_1[_a];
         counts[num]++;
     }
-    // create a merged and sorted array by iterating over the counting array
     var merged = [];
     for (var i = 0; i < counts.length; i++) {
         for (var j = 0; j < counts[i]; j++) {
--- pretest.ts
+++ pretest.ts
@@ -1,20 +1,20 @@
 function merge(collection_1 : number[],  collection_2 : number[]) : number[] {
     const max = Math.max(...collection_1, ...collection_2);
 
-    // create a counting array to keep track of how many times each number appears
+   
     const counts = new Array(max + 1).fill(0);
   
-    // count the number of times each number appears in collection_1
+    
     for (const num of collection_1) {
       counts[num]++;
     }
   
-    // count the number of times each number appears in collection_2
+    
     for (const num of collection_2) {
       counts[num]++;
     }
   
-    // create a merged and sorted array by iterating over the counting array
+    
     const merged: number[] = [];
     for (let i = 0; i < counts.length; i++) {
       for (let j = 0; j < counts[i]; j++) {
